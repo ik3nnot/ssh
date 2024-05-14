1b56d02e58d8c1044131dc7e3240a5d1d1b0ab39
ik3nnot <kkennethriparip@gmail.com>
2024-05-14 14:33:15 +0800

    TEST EDIT

--- Git.ps1
+++ Git.ps1
@@ -1,20 +1,20 @@
 # Change directory
-cd "C:\Users\shiyablue\Desktop\bon\ccnp21"
+cd "C:\Users\Administrator\Desktop\RIVAN\ssh"
 
 # Set user email
-git config --global user.email "vonherbertfaborada@gmail.com"
+git config --global user.email "kkennethriparip@gmail.com"
 
 # Set user name
-git config --global user.name "rivan16"
+git config --global user.name "ik3nnot"
 
 # Add all changes
 git add .
 
 # Commit changes
-git commit -m "CCNp21 Day 3"
+git commit -m "TEST EDIT"
 
 # Add remote repository
-git remote add origin https://github.com/rivan16/ccnp41.git
+git remote add origin https://github.com/ik3nnot/ssh.git
 
 # Set default branch
 git config --global init.defaultBranch main
